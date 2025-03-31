713610f4a088ebc13d8057a309fe7fbb02af9b1f
Minli Liao <166613619+MinliLiao@users.noreply.github.com>
2025-03-31 21:48:56 +0100

    Add x264 input file generation for cross compiling on x86 to script

--- AE_scripts/build_benchmarks.sh
+++ AE_scripts/build_benchmarks.sh
@@ -1,5 +1,6 @@
 # For SPEC17, place your ISO file in the root of the repository
 make -f Makefile_AE build_SPEC17
 make -f Makefile_AE setup_SPEC17
+make -f Makefile_AE x264_input
 # make -f Makefile_AE build_GAPBS
 # make -f Makefile_AE build_PARSEC
